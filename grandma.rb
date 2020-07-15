7e2c8c29c1b1a1051a4e7f1c22882ab00a60b66b
Jillian Weimer <jilliannweimer@gmail.com>
2020-07-15 12:09:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,19 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+phrase = gets.chomp
 
-# If you shout, she can hear you (or at least she thinks so) 
+if speak_to_grandma(phrase)
+  puts "HUH?! SPEAK UP, SONNY!"
+elseif == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN"
+else == "phrase.upcase"
+  puts "NO, NOT SINCE 1938!"
+
+
+
+
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
